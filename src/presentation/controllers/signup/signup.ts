ec692b866cae87c16a714154cf9e63ea79c086c7
Felipe Pereira <nfelipe.ps@gmail.com>
2021-11-05 12:54:01 -0300

    feat: ensure SignUpController returns new account on success

--- src/presentation/controllers/signup/signup.ts
+++ src/presentation/controllers/signup/signup.ts
@@ -1,24 +1,42 @@
+import { CreateAccount } from '../../../domain/usecases/create-account'
 import { InvalidParamError } from '../../errors/invalid-param-error'
 import { MissingParamError } from '../../errors/missing-param-error'
-import { badRequest } from '../../helpers/http-helpers'
+import { badRequest, ok, serverError } from '../../helpers/http-helpers'
 import { Controller } from '../../protocols/controller'
 import { HttpRequest, HttpResponse } from '../../protocols/http'
 
 export class SignUpController implements Controller {
+  private readonly createAccount: CreateAccount
+
+  constructor (createAccount: CreateAccount) {
+    this.createAccount = createAccount
+  }
+
   async handle (request: HttpRequest): Promise<HttpResponse> {
-    const { body } = request
-    const requiredFields = ['login', 'password', 'passwordConfirmation']
+    try {
+      const { body } = request
+      const requiredFields = ['login', 'password', 'passwordConfirmation']
 
-    for (const field of requiredFields) {
-      if (!body[field]) {
-        return badRequest(new MissingParamError(field))
+      for (const field of requiredFields) {
+        if (!body[field]) {
+          return badRequest(new MissingParamError(field))
+        }
       }
-    }
+      const { login, password, passwordConfirmation } = body
 
-    if (body.password !== body.passwordConfirmation) {
-      return badRequest(new InvalidParamError('passwordConfirmation'))
-    }
+      if (password !== passwordConfirmation) {
+        return badRequest(new InvalidParamError('passwordConfirmation'))
+      }
 
-    return await Promise.resolve(null)
+      const account = await this.createAccount.create({
+        login,
+        password
+      })
+
+      return ok(account)
+    } catch (error) {
+      console.error(error.message)
+      return serverError()
+    }
   }
 }
--- src/presentation/helpers/http-helpers.ts
+++ src/presentation/helpers/http-helpers.ts
@@ -1,8 +1,23 @@
+import { InternalServerError } from '../errors/internal-server-error'
 import { HttpResponse } from '../protocols/http'
 
 export const badRequest = (error: Error): HttpResponse => {
   return {
     statusCode: 400,
     body: error
+  }
+}
+
+export const ok = (data: any): HttpResponse => {
+  return {
+    statusCode: 200,
+    body: data
+  }
+}
+
+export const serverError = (): HttpResponse => {
+  return {
+    statusCode: 500,
+    body: new InternalServerError()
   }
 }
