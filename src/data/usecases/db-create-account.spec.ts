75e22a61d4ff14923664fbd908b8f8e2f567f586
Felipe Pereira <nfelipe.ps@gmail.com>
2021-11-07 14:52:47 -0300

    test: ensure DbCreateAccount returns account created

--- src/data/usecases/db-create-account.spec.ts
+++ src/data/usecases/db-create-account.spec.ts
@@ -1,8 +1,10 @@
-import { CreateAccount } from '../../domain/usecases/create-account'
+import { Account } from '../../domain/models/account'
+import { CreateAccount, CreateAccountModel } from '../../domain/usecases/create-account'
+import { CreateAccountRepository } from '../protocols/add-account-repository'
 import { Encrypter } from '../protocols/encrypter'
 import { DbCreateAccount } from './db-create-account'
 
-const makeEncryptStub = (): Encrypter => {
+const makeEncrypt = (): Encrypter => {
   class EncrypterStub implements Encrypter {
     async encrypt (data: string): Promise<string> {
       return await Promise.resolve('hash_password')
@@ -12,18 +14,35 @@ const makeEncryptStub = (): Encrypter => {
   return new EncrypterStub()
 }
 
+const makeCreateAccountRepository = (): CreateAccountRepository => {
+  class CreateAccountRepositoryStub implements CreateAccountRepository {
+    async create (accountData: CreateAccountModel): Promise<Account> {
+      return await Promise.resolve({
+        id: 'any_id',
+        login: 'any_login',
+        password: 'hash_password'
+      })
+    }
+  }
+
+  return new CreateAccountRepositoryStub()
+}
+
 interface SutTypes {
   sut: CreateAccount
   encrypter: Encrypter
+  createAccountRepository: CreateAccountRepository
 }
 
 const makeSut = (): SutTypes => {
-  const encrypter = makeEncryptStub()
-  const sut = new DbCreateAccount(encrypter)
+  const encrypter = makeEncrypt()
+  const createAccountRepository = makeCreateAccountRepository()
+  const sut = new DbCreateAccount(encrypter, createAccountRepository)
 
   return {
     sut,
-    encrypter
+    encrypter,
+    createAccountRepository
   }
 }
 
@@ -38,5 +57,35 @@ describe('DbCreateAccount Usecase', () => {
     })
 
     expect(encryptSpy).toBeCalledWith('any_password')
+  })
+
+  test('should call create with correct values', async () => {
+    const { sut, createAccountRepository } = makeSut()
+    const createSpy = jest.spyOn(createAccountRepository, 'create')
+
+    await sut.create({
+      login: 'any_login',
+      password: 'any_password'
+    })
+
+    expect(createSpy).toBeCalledWith({
+      login: 'any_login',
+      password: 'hash_password'
+    })
+  })
+
+  test('should return account created', async () => {
+    const { sut } = makeSut()
+
+    const account = await sut.create({
+      login: 'any_login',
+      password: 'any_password'
+    })
+
+    expect(account).toEqual({
+      id: 'any_id',
+      login: 'any_login',
+      password: 'hash_password'
+    })
   })
 })
