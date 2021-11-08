1fb8a0ba699e87a9d170b5fa7b549329b8cce314
Felipe Pereira <nfelipe.ps@gmail.com>
2021-11-08 17:31:48 -0300

    feat: ensure OrderTypeORMRepository returns order by id

--- src/infra/db/typeorm/repositories/order/order.ts
+++ src/infra/db/typeorm/repositories/order/order.ts
@@ -1,10 +1,11 @@
 import { getRepository } from 'typeorm'
 import { CreateOrderRepository } from '../../../../../data/protocols/create-order-repository'
+import { FindOrderRepository } from '../../../../../data/protocols/find-order-repository'
 import { Order } from '../../../../../domain/models/order'
 import { CreateOrderModel } from '../../../../../domain/usecases/create-order'
 import { OrderTypeORM } from '../../models/order'
 
-export class OrderTypeORMRepository implements CreateOrderRepository {
+export class OrderTypeORMRepository implements CreateOrderRepository, FindOrderRepository {
   async createBatch (ordersData: CreateOrderModel[]): Promise<Order[]> {
     const repository = getRepository(OrderTypeORM)
     const orders = repository.create(ordersData)
@@ -12,5 +13,16 @@ export class OrderTypeORMRepository implements CreateOrderRepository {
     await repository.save(orders)
 
     return orders
+  }
+
+  async find (id: string): Promise<Order> {
+    const repository = getRepository(OrderTypeORM)
+    const order = await repository.findOne({
+      where: {
+        id
+      }
+    })
+
+    return order
   }
 }
